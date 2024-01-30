16ca77fd777d9a129a8dbe8fd41d894bca25f86f
Maulishri <maulishribhandari@gmail.com>
2024-01-30 16:47:29 -0500

    Changed styling of about page

--- src/data.js
+++ src/data.js
@@ -5,91 +5,89 @@
 const cpp = "https://raw.githubusercontent.com/devicons/devicon/master/icons/cplusplus/cplusplus-original.svg"
 const python = "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg"
 
-export const user={
+export const user = {
     "name": "Maulishri Bhandari",
-    "profession": "Software Engineer",
-    "desc1": "I would describe myself as an individual who embraces challenges and can perform well under pressure. My objective is to build sustainable, trustworthy technology with an empathetic touch! I believe in working for society, and I love meeting new people." ,
-    "desc2": "Feel free to browse through my projects on GitHub, my art on Instagram, and my experiences on LinkedIn. Happy to connect!"
+    "profession": "Student at Georgia Tech",
+    "desc1": "I'm a passionate computer science student with a focus on machine learning, human-computer interaction, and IoT. Currently pursuing a Master of Science in Computer Science at the Georgia Institute of Technology, I'm dedicated to advancing the field of technology through innovation and impactful projects.",
+    "desc2": "I'm excited about the possibilities technology offers to shape the future, and I'm always eager to connect with like-minded professionals and collaborate on projects that make a difference. Let's connect and explore new horizons in the world of building empathetic technology together!",
+    "desc3": "Feel free to browse through my projects on GitHub, my art on Instagram, and my experiences on LinkedIn. Happy to connect!"
 }
 
-export const education=[
+export const education = [
+
     {
-        "year":"2018-2022",
-        "degree":"Bachelor Of Technology",
-        "institute":"Vellore Institute of Technology",
+        "year": "2023-2025",
+        "degree": "Masters of Science in Computer Science",
+        "institute": "Georgia Institute of Technology",
     },
+
     {
-        "year":"2016-2018",
-        "degree":"Higher Secondary Education",
-        "institute":"Podar School",
-    },
-    {
-        "year":"2004-2016",
-        "degree":"Secondary Education",
-        "institute":"Arya Vidya Mandir",
+        "year": "2018-2022",
+        "degree": "Bachelor Of Technology",
+        "institute": "Vellore Institute of Technology",
     }
 ]
 
 
-export const project=[
+export const project = [
     {
         "id": "1",
-        "img":"https://img.freepik.com/free-vector/flat-online-medical-conference-illustration_23-2148901289.jpg?size=626&ext=jpg&uid=R86918502&ga=GA1.2.1995763836.1670155431&semt=sph",
-        "title":"Patient Health Monitoring",
-        "desc":"An attempt to make the patient monitoring system easier given the doctor to patient ratio and unavailability of enough nursing staff",
-        "link":"https://github.com/Maulishri/Patient-Health-Montoring",
-        "tech":cpp
+        "img": "https://img.freepik.com/free-vector/flat-online-medical-conference-illustration_23-2148901289.jpg?size=626&ext=jpg&uid=R86918502&ga=GA1.2.1995763836.1670155431&semt=sph",
+        "title": "Patient Health Monitoring",
+        "desc": "An attempt to make the patient monitoring system easier given the doctor to patient ratio and unavailability of enough nursing staff",
+        "link": "https://github.com/Maulishri/Patient-Health-Montoring",
+        "tech": cpp
     },
 
     {
         "id": "2",
-        "img":"https://img.freepik.com/free-vector/flat-design-pitbull-pack_23-2148957816.jpg?size=338&ext=jpg&uid=R86918502&ga=GA1.2.1995763836.1670155431&semt=sph",
-        "title":"Dog Breed Classification",
-        "desc":"A RNN that identifies the picture of a dog you input. It identifies it as one of the many it has been trained. If the picture you provide is not present in the sets for it has been trained on, It shows the one with closest resemblence.",
-        "link":"https://github.com/Maulishri/Dog-Breed-Classifiacation",
-        "tech":python
+        "img": "https://img.freepik.com/free-vector/flat-design-pitbull-pack_23-2148957816.jpg?size=338&ext=jpg&uid=R86918502&ga=GA1.2.1995763836.1670155431&semt=sph",
+        "title": "Dog Breed Classification",
+        "desc": "A RNN that identifies the picture of a dog you input. It identifies it as one of the many it has been trained. If the picture you provide is not present in the sets for it has been trained on, It shows the one with closest resemblence.",
+        "link": "https://github.com/Maulishri/Dog-Breed-Classifiacation",
+        "tech": python
     },
 
     {
         "id": "3",
-        "img":"https://img.freepik.com/premium-vector/interpreting-human-gesures-as-commands-gesture-recognition-gestures-commands-handsfree-control_566886-3250.jpg?size=626&ext=jpg&uid=R86918502&ga=GA1.2.1995763836.1670155431&semt=sph",
-        "title":"Human_Activity_Recognition",
-        "desc":"A new and computationally cheap method to perform human activity recognition using PoseNet and LSTM. Where we use PoseNet for Preprocessing and LSTM for understand the sequence.",
-        "link":"https://github.com/Maulishri/Human_Activity_Recognition",
-        "tech":python
+        "img": "https://img.freepik.com/premium-vector/interpreting-human-gesures-as-commands-gesture-recognition-gestures-commands-handsfree-control_566886-3250.jpg?size=626&ext=jpg&uid=R86918502&ga=GA1.2.1995763836.1670155431&semt=sph",
+        "title": "Human_Activity_Recognition",
+        "desc": "A new and computationally cheap method to perform human activity recognition using PoseNet and LSTM. Where we use PoseNet for Preprocessing and LSTM for understand the sequence.",
+        "link": "https://github.com/Maulishri/Human_Activity_Recognition",
+        "tech": python
     },
 
     {
         "id": "4",
-        "img":"https://img.freepik.com/free-vector/human-hand-gestures-set_74855-5874.jpg?size=626&ext=jpg&uid=R86918502&ga=GA1.2.1995763836.1670155431&semt=sph",
-        "title":"Hand-Recongnition",
-        "desc":"A program to identify the presence of a human hand and the number of fingers being held up",
-        "link":"https://github.com/Maulishri/Hand-Recognition",
-        "tech":python
+        "img": "https://img.freepik.com/free-vector/human-hand-gestures-set_74855-5874.jpg?size=626&ext=jpg&uid=R86918502&ga=GA1.2.1995763836.1670155431&semt=sph",
+        "title": "Hand-Recongnition",
+        "desc": "A program to identify the presence of a human hand and the number of fingers being held up",
+        "link": "https://github.com/Maulishri/Hand-Recognition",
+        "tech": python
     },
     {
         "id": "5",
-        "img":"https://img.freepik.com/free-vector/delivery-bike-online-cartoon-character-send-order-location-sharing-smartphone-smart-city-transportation-concept-flat-vector-illustration_1150-58651.jpg?size=626&ext=jpg&uid=R86918502&ga=GA1.2.1995763836.1670155431&semt=sph",
-        "title":"Bike Sharing",
-        "desc":"A neural network to predict daily bike rental ridership from the given dataset. Decions made on the data analysis and visualization results.",
-        "link":"https://github.com/Maulishri/Bike-Sharing",
-        "tech":python
+        "img": "https://img.freepik.com/free-vector/delivery-bike-online-cartoon-character-send-order-location-sharing-smartphone-smart-city-transportation-concept-flat-vector-illustration_1150-58651.jpg?size=626&ext=jpg&uid=R86918502&ga=GA1.2.1995763836.1670155431&semt=sph",
+        "title": "Bike Sharing",
+        "desc": "A neural network to predict daily bike rental ridership from the given dataset. Decions made on the data analysis and visualization results.",
+        "link": "https://github.com/Maulishri/Bike-Sharing",
+        "tech": python
     },
     {
         "id": "6",
-        "img":"https://img.freepik.com/free-vector/compost-cycle-concept-illustration_114360-8733.jpg?size=338&ext=jpg&uid=R86918502&ga=GA1.2.1995763836.1670155431&semt=sph",
-        "title":"Mess-folio",
-        "desc":"It is an optimized way of organization to ensure less wastage of food",
-        "link":"https://github.com/Maulishri/Mess-folio",
-        "tech":python
+        "img": "https://img.freepik.com/free-vector/compost-cycle-concept-illustration_114360-8733.jpg?size=338&ext=jpg&uid=R86918502&ga=GA1.2.1995763836.1670155431&semt=sph",
+        "title": "Mess-folio",
+        "desc": "It is an optimized way of organization to ensure less wastage of food",
+        "link": "https://github.com/Maulishri/Mess-folio",
+        "tech": python
     }
 ]
 
-export const contact={
-    "email":"maulishribhandari@gmail.com",
-    "linkedIn":"https://www.linkedin.com/in/maulishri-bhandari-41b172190/",
-    "userID":"maulishri-bhandari-41b172190",
-    "github":"https://github.com/Maulishri",
-    "address":"Mumbai,Maharashtra,India",
+export const contact = {
+    "email": "maulishribhandari@gmail.com",
+    "linkedIn": "https://www.linkedin.com/in/maulishri-bhandari-41b172190/",
+    "userID": "maulishri-bhandari-41b172190",
+    "github": "https://github.com/Maulishri",
+    "address": "Atlanta, USA",
     "instagram": "https://instagram.com/mauli_ed.creations?igshid=YmMyMTA2M2Y="
 }
