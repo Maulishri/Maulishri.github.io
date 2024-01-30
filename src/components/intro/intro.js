2bb179b991db65303c9578cec25f7ba72468c14b
Maulishri <maulishribhandari@gmail.com>
2024-01-30 16:43:54 -0500

    Updated Introduction Page

--- src/components/intro/intro.js
+++ src/components/intro/intro.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './intro.css'
-import {user,contact} from '../../data'
+import { user, contact } from '../../data'
 import Profile from '../../image/profile.jpeg'
 
 let github = "https://img.icons8.com/fluency/2x/github.png"
@@ -9,34 +9,35 @@ let linkedIn = "https://img.icons8.com/color/2x/linkedin-circled--v4.png"
 
 
 export default function intro() {
-  return (
-    <div className='intro'>
-        <div className='i-left'>
-            <div className='i-profile'>
-                <img className="i-img" src={Profile} alt='Profile Image'></img>
+    return (
+        <div className='intro'>
+            <div className='i-left'>
+                <div className='i-profile'>
+                    <img className="i-img" src={Profile} alt='Profile Image'></img>
+                </div>
             </div>
-        </div>
 
-        <div className='i-right'>
-            <div className='i-right-wrapper'>
-                <div className='i-intro'>{user.profession}</div>
-                <div className='i-name'>{user.name}</div>
-                <div className="desc">{user.desc1}</div>
-                <div className="desc">{user.desc2}</div>
-                <div className='i-button-wrapper'>
-                    <div className='i-a'>
-                        <a className="i-icon" href={contact.github}><img alt="GitHub Profile" src={github} ></img></a>
+            <div className='i-right'>
+                <div className='i-right-wrapper'>
+                    <div className='i-intro'>{user.profession}</div>
+                    <div className='i-name'>{user.name}</div>
+                    <div className="desc">{user.desc1}</div>
+                    <div className="desc">{user.desc2}</div>
+                    <div className="desc">{user.desc3}</div>
+                    <div className='i-button-wrapper'>
+                        <div className='i-a'>
+                            <a className="i-icon" href={contact.github}><img alt="GitHub Profile" src={github} ></img></a>
+                        </div>
+                        <div className='i-a'>
+                            <a className="i-icon" href={contact.instagram}><img alt="Instagram Art" src={instagram}></img></a>
+                        </div>
+                        <div className='i-a'>
+                            <a className="i-icon" href={contact.linkedIn}><img alt="LinkedIn Profile" src={linkedIn}></img></a>
+                        </div>
                     </div>
-                    <div className='i-a'>
-                        <a className="i-icon" href={contact.instagram}><img alt="Instagram Art" src={instagram}></img></a>
-                    </div>
-                    <div className='i-a'>
-                        <a className="i-icon" href={contact.linkedIn}><img alt="LinkedIn Profile" src={linkedIn}></img></a>
-                    </div>
-                </div>            
+                </div>
             </div>
-        </div>
 
-    </div>
-  )
+        </div>
+    )
 }
